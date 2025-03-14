2b882f2501673ab5ab74b387e2d051267597840b
Nayuki <me@nayuki.cyou>
2025-03-14 19:25:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
   serverId: '123456789012345678',
   channelId: '123456789012345678',
-  url: '# Your YouTube URL here',
+  url: '# Your Radio Audio Stream URL m3u8 or any',
   volume: 100, // 0-100
 };
